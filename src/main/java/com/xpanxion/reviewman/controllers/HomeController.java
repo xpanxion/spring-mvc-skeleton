ea0245db4027d09fe05e47b7e1a7a04c5d5fd901
Brian Smith <brian.darklance@gmail.com>
2013-09-25 22:49:54 -0500

    Creating inital skeliton using standard spring setup This is a basic spring mvc setup

---
+++ src/main/java/com/xpanxion/reviewman/controllers/HomeController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.xpanxion.reviewman.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author brian
+ *
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping("**/home")
+	public ModelAndView getHomePage(){
+		ModelAndView mAndV = new ModelAndView("home");
+		return mAndV;
+	}
+	
+}
---
+++ src/main/webapp/jsp/home.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+<h1>Under Construction!!!</h1>
+</body>
+</html>
