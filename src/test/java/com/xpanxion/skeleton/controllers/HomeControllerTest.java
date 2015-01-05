d95689021c1e5423624ddeda88df557acc6cc4f1
Gordon Child <gchild@xpanxion.com>
2015-01-05 11:02:50 -0700

    Modified HomeControllerTest to use mocking
      - Test sets up service to produce mocked beans
      - Test verifies that the Controller produces
        correct model

--- src/test/java/com/xpanxion/skeleton/controllers/HomeControllerTest.java
+++ src/test/java/com/xpanxion/skeleton/controllers/HomeControllerTest.java
@@ -3,6 +3,7 @@
  */
 package com.xpanxion.skeleton.controllers;
 
+import com.xpanxion.skeleton.dto.beans.TestBean;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +14,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.xpanxion.skeleton.controllers.HomeController;
 import com.xpanxion.skeleton.service.TestService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 
 /**
  * @author brian
@@ -31,8 +37,26 @@ public class HomeControllerTest {
      */
     @Test
     public void testGetHomePage() {
+        // given
+        TestBean bean1 = new TestBean();
+        bean1.setId(1);
+        bean1.setText("Text1");
+
+        TestBean bean2 = new TestBean();
+        bean2.setId(2);
+        bean2.setText("Text2");
+
+        List<TestBean> beanList = new ArrayList<>();
+        beanList.add(bean1);
+        beanList.add(bean2);
+        given(testService.getTestBeans()).willReturn(beanList);
+
+        // when
         ModelAndView mAndV = this.testee.getHomePage();
+
+        // then
         Assert.assertEquals("home", mAndV.getViewName());
+        Assert.assertEquals(beanList,mAndV.getModel().get("test"));
     }
 
 }
